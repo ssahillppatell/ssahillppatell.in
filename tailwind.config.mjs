d656ca8a0d49e1e88c47366500fc89383c043f24
Sahil Patel <ssahillppatell@gmail.com>
2023-10-24 18:52:50 +0530

    Layout setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['Josefin Sans', 'sans-serif', 'system-ui'],
+		},
+		extend: {
+			colors: {
+				offWhite: '#FAF9F6',
+				primary: '#5B98F4'
+			}
+		},
 	},
 	plugins: [],
 }
