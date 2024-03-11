a73b09a9de718a5684de047a8d137627d1c45759
Sahil Patel <ssahillppatell@gmail.com>
2024-03-11 22:41:37 +0530

    Saving before test release

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
 
+const isProd = import.meta.env.PROD;
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue()],
+  site: isProd ? "https://ssahillppatell.github.io'" : "http://localhost:4321",
+  base: isProd ? "/ssahillppatell.in/" : "/",
 });
