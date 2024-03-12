2043a5337cc69a0770d2ee4a50564f0060b5f9cd
Sahil Patel <ssahillppatell@gmail.com>
2024-03-12 21:24:43 +0530

    Config change as per the custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
 
-const isProd = import.meta.env.PROD;
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  site: isProd ? "https://ssahillppatell.github.io/ssahillppatell.in/" : "http://localhost:4321"
+  site: 'https://ssahillppatell.in'
 });
